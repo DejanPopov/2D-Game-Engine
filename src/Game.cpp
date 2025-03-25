7141a26ef9e39a124d581266409437f55f731021
Dejan <dekipopica92@gmail.com>
2025-03-25 09:35:10 +0100

    Update Game.cpp
    - Initialize function now handles window opening, rendering and error handling

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game()
@@ -13,7 +14,46 @@ Game::~Game()
 
 void Game::Initialize()
 {
+    // We want to initialise everything
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+    {
+        // return from this function dont do anything
+        std::cerr << "Error Initializing SDL" << std::endl;
+        return;
+    }
+    // Raw pointer to the struct ( SDL_Window*)
+    // window is going to be a pointer to adress where struct is allocated
+    SDL_Window* window = SDL_CreateWindow
+    (
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
 
+    // If we could not create window
+    if(!window)
+    {
+        std::cerr << "Error Creating SDL Window" << std::endl;
+        return;
+    }
+
+    // -1 means use default monitor
+    SDL_Renderer* renderer = SDL_CreateRenderer
+    (
+        window,
+        -1, 
+        0
+    );
+
+    // If we could not create renderer
+    if(!renderer)
+    {
+        std::cerr << "Error Creating SDL Renderer" << std::endl;
+        return;
+    }
 }
 
 void Game::Run()

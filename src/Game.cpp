e23eab5212746940a8d882e738de6eec1a4b41f2
Dejan <dekipopica92@gmail.com>
2025-04-02 16:27:39 +0200

    Update ProcessInput method to handle pressed keys

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,6 +4,7 @@
 
 Game::Game()
 {
+    isRunning = false;
     std::cout << "Game Constructor called!" << std::endl;
 }
 
@@ -21,19 +22,12 @@ void Game::Initialize()
         std::cerr << "Error Initializing SDL" << std::endl;
         return;
     }
+
     // Raw pointer to the struct ( SDL_Window*)
     // window is going to be a pointer to adress where struct is allocated
-    SDL_Window* window = SDL_CreateWindow
-    (
-        NULL,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_BORDERLESS
-    );
+    window = SDL_CreateWindow(NULL,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_BORDERLESS);
 
-    // If we could not create window
+    // If we could not create window, we are testing if window is NULL
     if(!window)
     {
         std::cerr << "Error Creating SDL Window" << std::endl;
@@ -41,12 +35,7 @@ void Game::Initialize()
     }
 
     // -1 means use default monitor
-    SDL_Renderer* renderer = SDL_CreateRenderer
-    (
-        window,
-        -1, 
-        0
-    );
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
     // If we could not create renderer
     if(!renderer)
@@ -54,16 +43,37 @@ void Game::Initialize()
         std::cerr << "Error Creating SDL Renderer" << std::endl;
         return;
     }
-}
 
-void Game::Run()
-{
-
+    isRunning = true;
 }
 
 void Game::ProcessInput()
 {
+    // Check if user presses something on keyboard (ESC)
+    SDL_Event sdlEvent;
 
+    // & Get the adress a memory in struct, so its not passing entire struct without copying
+    while(SDL_PollEvent(&sdlEvent))
+    {
+        switch (sdlEvent.type)
+        {  
+            // If users clicks X on software window
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    isRunning = false;
+                }
+                break;
+
+
+
+
+        }
+    }
 }
 
 void Game::Update()
@@ -76,7 +86,22 @@ void Game::Render()
 
 }
 
+void Game::Run()
+{
+    while (isRunning)
+    {
+        ProcessInput();
+        Update();
+        Render();
+    }
+}
+
 void Game::Destroy()
 {
+    // Clean up after Destroy is called
+    // Clean-up is done in inverse order that we created
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 }
--- src/Game.h
+++ src/Game.h
@@ -2,10 +2,15 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SDL2/SDL.h>
+
 class Game
 {
     private:
-        //....
+    
+        SDL_Window*   window;
+        SDL_Renderer* renderer;
+        bool isRUnning;
 
     public:
 
